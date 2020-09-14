eb41c9d3a8cdaebc057e09d573ef00132294cd9b
Joni Soropia <jasoropia@gmail.com>
2020-09-13 21:00:05 -0400

    Updates

---
+++ precache-manifest.b9b2c8a354cb127a55a15835bef1ff07.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9132ef5767c522651c85680ca05ed0e4",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "2168532f651cfcbe4812",
+    "url": "/mywebsite/static/css/main.e127aab0.chunk.css"
+  },
+  {
+    "revision": "93ee9ad29c6cc9b2c8ba",
+    "url": "/mywebsite/static/js/2.922e3714.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mywebsite/static/js/2.922e3714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2168532f651cfcbe4812",
+    "url": "/mywebsite/static/js/main.ba386eea.chunk.js"
+  },
+  {
+    "revision": "8cd8fe806c906a85fe43",
+    "url": "/mywebsite/static/js/runtime-main.8f22862e.js"
+  },
+  {
+    "revision": "deb1dd8deaf600030295bd129468b140",
+    "url": "/mywebsite/static/media/Cormorant-Bold.deb1dd8d.otf"
+  },
+  {
+    "revision": "5d0684caf9aff9baee3a21c0d05c664d",
+    "url": "/mywebsite/static/media/ITCKRIST.5d0684ca.ttf"
+  },
+  {
+    "revision": "845a73cf6a5f907a25b735cda2caddc8",
+    "url": "/mywebsite/static/media/Jonathan.845a73cf.ttf"
+  },
+  {
+    "revision": "cca009062aa746ab201a5b6a0acef974",
+    "url": "/mywebsite/static/media/aesthetics.cca00906.jpg"
+  },
+  {
+    "revision": "37f6e7431636bf3483f67d25fb9556af",
+    "url": "/mywebsite/static/media/avatar.37f6e743.png"
+  },
+  {
+    "revision": "bbd8cc8a8a726e759911ceb184b29d8c",
+    "url": "/mywebsite/static/media/award.bbd8cc8a.png"
+  },
+  {
+    "revision": "c7261a2bd4497773e7c3ddc5060bc1f9",
+    "url": "/mywebsite/static/media/book.c7261a2b.png"
+  },
+  {
+    "revision": "89eb3fa1a44903bc75663c05e8b90242",
+    "url": "/mywebsite/static/media/buildings.89eb3fa1.png"
+  },
+  {
+    "revision": "226a92fae0f371f9d6dbb42f9e765fbb",
+    "url": "/mywebsite/static/media/buy.226a92fa.png"
+  },
+  {
+    "revision": "32ec1d02983d10d4571a15c6b0b80fc3",
+    "url": "/mywebsite/static/media/cherry.32ec1d02.jpeg"
+  },
+  {
+    "revision": "16de66a8d87c5b216606099fcd2e3378",
+    "url": "/mywebsite/static/media/cloud.16de66a8.jpg"
+  },
+  {
+    "revision": "3b1986bad74dfb3a5948ede2d65edb01",
+    "url": "/mywebsite/static/media/collage.3b1986ba.jpg"
+  },
+  {
+    "revision": "f621961a85e28d5d5105ee90b6907ce4",
+    "url": "/mywebsite/static/media/comp.f621961a.png"
+  },
+  {
+    "revision": "29da0d9e604ecd3b5d609ce7d44049ee",
+    "url": "/mywebsite/static/media/design.29da0d9e.png"
+  },
+  {
+    "revision": "69465397acd9061d53a969919c159d10",
+    "url": "/mywebsite/static/media/desk.69465397.png"
+  },
+  {
+    "revision": "6c59f42dafa0b2ac97b4f477aead265f",
+    "url": "/mywebsite/static/media/evaluate.6c59f42d.png"
+  },
+  {
+    "revision": "e6263dde724236f8d6e07bbbd2dae39a",
+    "url": "/mywebsite/static/media/game.e6263dde.png"
+  },
+  {
+    "revision": "8c26d4f56efd308753a9907c99e0719a",
+    "url": "/mywebsite/static/media/gb.8c26d4f5.png"
+  },
+  {
+    "revision": "670f5b11b14511a6ea43ae478153c900",
+    "url": "/mywebsite/static/media/highpark.670f5b11.jpg"
+  },
+  {
+    "revision": "c9b4e11e9064ba813f1a3d814ee51b6e",
+    "url": "/mywebsite/static/media/inspect.c9b4e11e.png"
+  },
+  {
+    "revision": "9c807d155f62e2ef9f5811e1eb4fd820",
+    "url": "/mywebsite/static/media/joniprofile.9c807d15.jpg"
+  },
+  {
+    "revision": "afb34d903f74f3c8763c0d237974a587",
+    "url": "/mywebsite/static/media/marketing.afb34d90.png"
+  },
+  {
+    "revision": "15e16f2cc843738fded8def4feba0dc4",
+    "url": "/mywebsite/static/media/outreach.15e16f2c.png"
+  },
+  {
+    "revision": "2a9fbdb1d470b43fb405e49c002965a2",
+    "url": "/mywebsite/static/media/people.2a9fbdb1.png"
+  },
+  {
+    "revision": "ea36ef745fc6362c074c17d594b1027f",
+    "url": "/mywebsite/static/media/phone.ea36ef74.png"
+  },
+  {
+    "revision": "42b875adac71b18c7083226066809a56",
+    "url": "/mywebsite/static/media/pinkbrick.42b875ad.jpg"
+  },
+  {
+    "revision": "fbda6b4554643e93d1e8e432aee0216c",
+    "url": "/mywebsite/static/media/pinkmarble.fbda6b45.jpeg"
+  },
+  {
+    "revision": "92ff933ce85604b065a77a2d729ff986",
+    "url": "/mywebsite/static/media/plan.92ff933c.png"
+  },
+  {
+    "revision": "fdde6f64e06c76592f3fe0d5350d396b",
+    "url": "/mywebsite/static/media/recruit.fdde6f64.png"
+  },
+  {
+    "revision": "224287263e0bdc914d9ce0bece02411f",
+    "url": "/mywebsite/static/media/scholar.22428726.png"
+  },
+  {
+    "revision": "b329d6c3e809fabcb88a5f3cd255e737",
+    "url": "/mywebsite/static/media/signature.b329d6c3.png"
+  },
+  {
+    "revision": "aa76906d9555666da6bc82c7deb568d3",
+    "url": "/mywebsite/static/media/special.aa76906d.png"
+  },
+  {
+    "revision": "bf911f853f40634f4bdea6e713506678",
+    "url": "/mywebsite/static/media/train.bf911f85.png"
+  },
+  {
+    "revision": "8109dcd8aa6ba46f49d2654398d06ada",
+    "url": "/mywebsite/static/media/uoft.8109dcd8.png"
+  },
+  {
+    "revision": "b9e8ccdde84ca4c2a61452d7d7937fac",
+    "url": "/mywebsite/static/media/whitefloral.b9e8ccdd.jpg"
+  },
+  {
+    "revision": "fc5d524a7ffd2797aefbf5338f9b489d",
+    "url": "/mywebsite/static/media/whitemarble.fc5d524a.jpg"
+  },
+  {
+    "revision": "38131d1d30f1e32132316329e3c0928b",
+    "url": "/mywebsite/static/media/whitetable.38131d1d.jpg"
+  }
+]);
