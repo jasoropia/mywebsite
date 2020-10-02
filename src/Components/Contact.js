b4bdb96f28a9c2488523119761190d6245514ef4
NazGy <nazargy@gmail.com>
2020-10-02 12:46:01 -0400

    fixed contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,23 +8,35 @@ import "../CSS/contact.css";
 class Contact extends React.Component {
     render() {
         return (
-            
-            <div class = 'conatactpage'>
-                <img src = {Cloud} class = 'cloud'/>
-                <div class = "contact1"></div>
-                <div class = "contact"> Contact Me </div>
-                <img src= {Collage} class= "collage"/>
 
-                <div class = 'information'>               
-                    <img src = {Mail} class = "mail"/>
-                    <p class = "email"> jasoropia@gmail.com </p>
-                    <img src = {Home} class = "home"/>
-                    <p class = "sauga"> Mississauga, ON </p>
-                    <img src ={Linkedin} class = "Linkedin"/>
-                    <p class = "linkedin"> JoniSoropia </p>
-                    </div> 
+            <div class='conatactpage'>
+                <img src={Cloud} class='cloud' />
+                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+                    <div class="contact"> Contact Me </div>
+                    <div class="contact1"></div>
+                    <img src={Collage} class="collage" />
+
+                    <div class='information'>
+                        <div class="row"> 
+                            <img src={Mail} class="mail" />
+                            <p class="email"> jasoropia@gmail.com </p>
+                        </div>
+                        <div  class="row">
+                            <img src={Home} class="home" />
+                            <p class="sauga"> Mississauga, ON </p>
+                        </div>
+                        <div  class="row">
+                            <img src={Linkedin} class="Linkedin" />
+                            <p class="linkedin"> JoniSoropia </p>
+                        </div>
+                    </div>
+
+                </div>
+
+
+
             </div>
-           
+
         )
 
     }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,41 +1,41 @@
 import React from 'react';
-import {withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import "../CSS/projects.css"
-import {ProImage1} from '../Images/recruit.png';
-import {ProImage2} from "../Images/inspect.png";
-import {ProImage3} from "../Images/outreach.png";
-import {ProImage4} from "../Images/plan.png";
-import {ProImage5} from "../Images/evaluate.png";
-import {ProImage6} from "../Images/design.png";
+import  ProImage1  from '../Images/recruit.png';
+import  ProImage2  from "../Images/inspect.png";
+import  ProImage3  from "../Images/outreach.png";
+import  ProImage4  from "../Images/plan.png";
+import  ProImage5  from "../Images/evaluate.png";
+import  ProImage6  from "../Images/design.png";
 import Divider from '@material-ui/core/Divider';
 
 class Projects extends React.Component {
     render() {
         return (
-            
-        <div class = "project">
-            <div class = 'top' style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%'}}>
-            <img src = {ProImage1} class= 'proimage'/>
-            <img src = {ProImage2} class= 'proimage'/>
-            <img src = {ProImage3} class= 'proimage'/>
+
+            <div class="project">
+                <div class='top' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+                    <img src={ProImage1} class='proimage' />
+                    <img src={ProImage2} class='proimage' />
+                    <img src={ProImage3} class='proimage' />
+                </div>
+
+                <div style={{ display: 'flex', justifyContent: 'space-around', width: '100%', marginTop: '2%' }}>
+                    <Divider className="divider2" style={{ backgroundColor: "#F4D8D4" }}></Divider>
+                    <Divider className="divider2" style={{ backgroundColor: "#D3BAB7" }}></Divider>
+                    <Divider className="divider2" style={{ backgroundColor: "#FF8AC2" }}></Divider>
+                    <Divider className="divider2" style={{ backgroundColor: "#700136" }}></Divider>
+                </div>
+
+                <div class='bottom' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+                    <img src={ProImage4} class='proimage' />
+                    <img src={ProImage5} class='proimage' />
+                    <img src={ProImage6} class='proimgae' />
+                </div>
+
+                <p class='title'>My Projects</p>
             </div>
 
-            <div style={{display: 'flex', justifyContent: 'space-around', width: '100%', marginTop: '2%'}}>
-            <Divider className="divider2" style={{backgroundColor: "#F4D8D4"}}></Divider>
-            <Divider className="divider2" style={{backgroundColor: "#D3BAB7"}}></Divider>
-            <Divider className="divider2" style={{backgroundColor: "#FF8AC2"}}></Divider>
-            <Divider className="divider2" style={{backgroundColor: "#700136"}}></Divider>
-            </div> 
-
-            <div class = 'bottom' style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%'}}>
-            <img src = {ProImage4} class= 'proimage'/>
-            <img src = {ProImage5} class= 'proimage'/>
-            <img src = {ProImage6} class= 'proimgae'/>
-            </div>
-        
-            <p class = 'title'>My Projects</p>
-        </div>
-           
         )
 
     }
